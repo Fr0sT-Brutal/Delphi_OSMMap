e888f7ff33cd861c61eb7c595b424ffa43bf1b0b
Fr0sT <fr0st@local>
2021-07-28 14:26:53 +0300

    OSM.TileStorage.pas: + TTileStorage.StoreTile, marked as raising exception on invalid (non-PNG) content. Check is performed before any save to cache. Ms argument is nulled anyway so is made "var".

--- Source/OSM.TileStorage.pas
+++ Source/OSM.TileStorage.pas
@@ -38,9 +38,9 @@ const
   DefaultTilePathPatt = '%d'+PathDelim+'%d'+PathDelim+'%d.png';
 
   {$IFDEF MSWINDOWS} //~ Only Windows has GDI handles limit
-  GDI_HANDLES_LIMIT = 3000; //< Default overall limit is 10k per process, choose reasonable number below this limit
-  GDI_PER_BMP = 1;          //< Number of GDI handles per `TBitmap`
-  GDI_PER_PNG = 3;          //< Number of GDI handles per `TPngImage`
+  GDI_HANDLES_LIMIT = 3000; // Default overall limit is 10k per process, choose reasonable number below this limit
+  GDI_PER_BMP = 1;          // Number of GDI handles per `TBitmap`
+  GDI_PER_PNG = 3;          // Number of GDI handles per `TPngImage`
   {$ENDIF}
 
 type
@@ -72,17 +72,17 @@ type
 
   // Flags for TTileStorage
   TTileStorageOption = (
-    tsoNoFileCache,        //< disable all file cache operations
-    tsoReadOnlyFileCache   //< disable write file cache operations
+    tsoNoFileCache,        // disable all file cache operations
+    tsoReadOnlyFileCache   // disable write file cache operations
   );
   TTileStorageOptions = set of TTileStorageOption;
 
   // Limits of tile cache in memory
   TCacheLimits = record
-    BmpCount: Cardinal;     //< Total count of bitmaps, memory occupied = `BmpCount*TILE_BITMAP_SIZE`
-    PngTotalSize: Cardinal; //< Total memory occupied by PNG cache
-    PngCount: Cardinal;     //< Total count of PNG objects
-    StmSize: Cardinal;      //< Total memory occupied by PNG stream cache
+    BmpCount: Cardinal;     // Total count of bitmaps, memory occupied = `BmpCount*TILE_BITMAP_SIZE`
+    PngTotalSize: Cardinal; // Total memory occupied by PNG cache
+    PngCount: Cardinal;     // Total count of PNG objects
+    StmSize: Cardinal;      // Total memory occupied by PNG stream cache
   end;
 
   // Class that encapsulates memory and file cache of tile images.
@@ -125,10 +125,12 @@ type
     //   @returns Bitmap or @nil if not available locally
     function GetTile(const Tile: TTile): TBitmap;
     // Adds memory stream `Ms`, PNG and bitmap produced from it to memory and file cache. @br
-    // ! **TileStorage takes ownership on `Ms` so it must not be freed** !
+    // ! **TileStorage takes ownership on `Ms` so caller should forget about it ** !
+    // @raises Exception if `Ms` doesn't contain valid PNG data
     //   @param Tile - tile to store
-    //   @param Ms - memory stream containing PNG image
-    procedure StoreTile(const Tile: TTile; Ms: TMemoryStream);
+    //   @param Ms - memory stream containing PNG image. When function returns, this \
+    //     reference gets nulled even if an exception has risen.
+    procedure StoreTile(const Tile: TTile; var Ms: TMemoryStream);
     // Empty all caches
     procedure ClearCache;
 
@@ -338,21 +340,33 @@ begin
   FBmpCache.Push(Tile, Result, TILE_BITMAP_SIZE);
 end;
 
-procedure TTileStorage.StoreTile(const Tile: TTile; Ms: TMemoryStream);
+procedure TTileStorage.StoreTile(const Tile: TTile; var Ms: TMemoryStream);
 var png: TPngImage;
 begin
+  // First try to load stream to PNG image to check if it's correct
+  Ms.Position := 0;
+  png := TPngImage.Create;
+  try
+    png.LoadFromStream(Ms);
+  except on E: Exception do
+    begin
+      FreeAndNil(png);
+      FreeAndNil(Ms); // We took ownership on Ms so clean it up
+      raise;
+    end;
+  end;
+
   // Save to disk as PNG
+  Ms.Position := 0;
   if ([tsoNoFileCache, tsoReadOnlyFileCache] * FOptions = []) then
     StoreInFileCache(Tile, Ms);
   // Store in stream cache
   FStmCache.Push(Tile, Ms, Ms.Size);
   // Store in PNG cache
-  Ms.Position := 0;
-  png := TPngImage.Create;
-  png.LoadFromStream(Ms);
   FPngCache.Push(Tile, png, Ms.Size);
   // Store in bitmap cache
   FBmpCache.Push(Tile, PNGtoBitmap(png), TILE_BITMAP_SIZE);
+  Ms := nil; // Nullify the reference
 end;
 
 procedure TTileStorage.ClearCache;
