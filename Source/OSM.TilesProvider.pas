5420eda222adf9111bfc02834364fb35914c274b
Fr0sT-Brutal <fr0st.brutal@gmail.com>
2021-10-07 11:03:51 +0300

    [API] + OSM.TilesProvider.pas, TTilesProvider, added property Properties to access provider-specific properties.

--- Source/OSM.TilesProvider.pas
+++ Source/OSM.TilesProvider.pas
@@ -12,12 +12,16 @@ unit OSM.TilesProvider;
 interface
 
 uses
+  SysUtils,
   OSM.SlippyMapUtils;
 
 type
   // Abstract base class for tile image provider. Real implementations must
   // inherit from it, assign properties and override methods.
   TTilesProvider = class
+  protected
+    function GetProperty(const Index: string): string; virtual;
+    procedure SetProperty(const Index, Value: string); virtual;
   public
     // Minimal zoom level. Usually `0`
     MinZoomLevel: TMapZoomLevel;
@@ -28,8 +32,13 @@ type
     TilesCopyright: string;
     // [opt] API key for requesting tiles
     APIKey: string;
+
     // Method to get URL of specified tile
     function GetTileURL(const Tile: TTile): string; virtual; abstract;
+
+    // Generic storage for provider-specific properties. Raises exception in
+    // base class, should be implemented in descendants
+    property Properties[const Index: string]: string read GetProperty write SetProperty;
   end;
 
   // Dummy tile provider class, used as a stub in map control if no real provider
@@ -41,6 +50,18 @@ type
   end;
 
 implementation
+
+{ TTilesProvider }
+
+function TTilesProvider.GetProperty(const Index: string): string;
+begin
+  raise Exception.Create('Getter for Properties not implemented');
+end;
+
+procedure TTilesProvider.SetProperty(const Index, Value: string);
+begin
+  raise Exception.Create('Setter for Properties not implemented');
+end;
 
 { TDummyTilesProvider }
 
